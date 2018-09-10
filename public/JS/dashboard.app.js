35a6ed46b6e3885c293738b3eb0637b302e58dba
ebklingl <ebklingl@iu.edu>
2018-09-10 11:20:57 -0400

    FIXED CODE - after class 9/10

--- public/JS/dashboard.app.js
+++ public/JS/dashboard.app.js
@@ -12,60 +12,22 @@ var dashboardApp = new Vue({
     tasks: [
       {
         "id": 1,
-        "title": "Build UI",
-        "type" : "Story",
-        "size" : "M",
-        "team" : "Noble Carrots",
-        "status": "Started",
-        "start_date": "2018-08-02",
+        "title": '',
+        "type" : '',
+        "size" : '',
+        "team" : '',
+        "status": '',
+        "start_date": '',
         "close_date": null,
-        "hours_worked":90,
-        "perc_complete": 95,
-        "current_sprint" : true
-      },
-      {
-        "id": 2,
-        "title": "Update unit tests",
-        "type" : "Story",
-        "size" : "L",
-        "team" : "MS Why Us",
-        "status": "Closed",
-        "start_date": "2018-07-15",
-        "close_date": "2018-08-1",
-        "hours_worked": 40,
-        "perc_complete": 100,
-        "current_sprint" : true
-      },
-      {
-        "id": 3,
-        "title": "Write middleware",
-        "type" : "Epic",
-        "size" : "XL",
-        "team" : "California Dream",
-        "status": "Open",
-        "start_date": null,
-        "close_date": null,
-        "hours_worked": 0,
-        "perc_complete": 0,
-        "current_sprint" : true
-      },
-      {
-        "id": 4,
-        "title": "Completion % not saving",
-        "type" : "Bug",
-        "size" : "XS",
-        "team" : "Luke's Parents",
-        "status": "Open",
-        "start_date": "2018-07-29",
-        "close_date": "",
-        "hours_worked": 27,
-        "perc_complete": 80,
-        "current_sprint" : false
+        "hours_worked": '',
+        "perc_complete": '',
+        "current_sprint" : ''
       }
     ]
   },
   computed: {
     days_left: function () {
+      //this.target_date
       return moment(this.target_date).diff(moment(), 'days')
     },
     pretty_target_date: function () {
@@ -77,24 +39,45 @@ var dashboardApp = new Vue({
     pretty_date: function (d) {
       return moment(d).format('l')
     },
+    log (msg) {
+      alert(msg);
+    },
+    fetchTasks() {
+      fetch( 'https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json' )
+      .then( response => response.json() )
+      // ^^ means the same as .then( function(response))
+      .then( function(json) {
+        dashboardApp.tasks = json;
+        console.log(json);
+        console.log(this.tasks);
+      })
+      .catch( function(err) {
+        console.log('FETCH ERROR:');
+        console.log(err);
+      })
+    },
     pretty_currency: function (val) {
       if (val < 1e3) {
         return '$ ' + val
       }
 
       if (val < 1e6) {
-        return '$ ' + (val/1e3).toFixed(1) + ' k'
+        return '$ ' + (val/1e3).toFixed(1) + ' k';
       }
 
-      return '$ ' + (val/1e6).toFixed(1) + ' M'
+      return '$ ' + (val/1e6).toFixed(1) + ' M';
     },
     completeClass: function(task) {
       if (task.perc_complete == 100 ) {
-        return 'alert-success'
+        return 'alert-success';
       }
       if (task.current_sprint && task.hours_worked == 0 ) {
-        return 'alert-warning'
+        return 'alert-warning';
       }
     }
+  }, //end methods
+  created () {
+    this.fetchTasks();
   }
+
 })
