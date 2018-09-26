75c51925db29351c318f0a310fcbb590357230cc
ebklingl <ebklingl@iu.edu>
2018-09-26 09:41:03 -0400

    final end of class 9/26

---
+++ public/api/team.php
@@ -0,0 +1,12 @@
+<?php
+
+require '../../common.php';
+
+$teams = Team::getAll();
+
+// 2. Convert to JSON
+$json = json_encode($teams, JSON_PRETTY_PRINT)
+
+// 3. Print
+header('Content-Type: application/json')
+echo $json;
