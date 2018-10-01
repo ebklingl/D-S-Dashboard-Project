c3a45161f6353279fb28f22f5571b9b6764687b6
ebklingl <ebklingl@iu.edu>
2018-10-01 11:28:10 -0400

    changes to work.php

--- public/api/work.php
+++ public/api/work.php
@@ -2,7 +2,8 @@
 require '../../app/common.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-  require 'workPost.php'
+  require 'workPost.php';
+  exit;
 }
 
 $taskId = intval($_GET['taskId'] ?? 0);
