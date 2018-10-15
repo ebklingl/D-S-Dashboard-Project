8dff1e0fe8e2640250c4bd761fd0bd76022d740c
ebklingl <ebklingl@iu.edu>
2018-10-15 11:13:05 -0400

    partial in-class (10/15) ... some code still needed from videos

--- public/JS/dashboard.app.js
+++ public/JS/dashboard.app.js
@@ -29,43 +29,44 @@ var dashboardApp = new Vue({
   },
   computed: {
     days_left: function () {
-      return moment(this.project.target_date).diff(moment(), 'days');
+      return moment(this.project.target_date).diff(moment(), 'days')
     }
   },
   methods: {
     pretty_date: function (d) {
-      return moment(d).format('l');
+      return moment(d).format('l')
     },
     pretty_currency: function (val) {
       if (val < 1e3) {
-        return '$ ' + val;
+        return '$ ' + val
       }
 
       if (val < 1e6) {
-        return '$ ' + (val/1e3).toFixed(1) + ' k';
+        return '$ ' + (val/1e3).toFixed(1) + ' k'
       }
 
-      return '$ ' + (val/1e6).toFixed(1) + ' M';
+      return '$ ' + (val/1e6).toFixed(1) + ' M'
     },
     completeClass: function(task) {
       if (task.perc_complete == 100 ) {
-        return 'alert-success';
+        return 'alert-success'
       }
       if (task.current_sprint && task.hours_worked == 0 ) {
-        return 'alert-warning';
+        return 'alert-warning'
       }
     },
-    fetchTasks () {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/video/app/data/p1-tasks.json')
+    fetchTasks (pid) {
+      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/app/data/p1-tasks.json')
       .then( response => response.json() )
+      // ^ This is the same as .then( function(response) {return response.json()} )
       .then( json => {dashboardApp.tasks = json} )
       .catch( err => {
         console.log('TASK FETCH ERROR:');
         console.log(err);
       })
     },
-    fetchProject () {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/video/app/data/work1.json')
+    fetchProject (pid) {
+      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/app/data/project1.json')
       .then( response => response.json() )
       .then( json => {dashboardApp.project = json} )
       .catch( err => {
@@ -73,42 +74,98 @@ var dashboardApp = new Vue({
         console.log(err);
       })
     },
-
-    fetchProjectWork () {
-      fetch('api/workHours.php?projectId=' +pid)
-      .then( function(response) {return response.json();} )
-      .then( json => {dashboardApp.workHours = json} )
-          dashboardApp.workHours = json;
-          this.formatWorkData();
-          //TODO: this.buildEffortChart();
-        })
-      },
+    fetchProjectWork (pid) {
+      fetch('api/workHours.php?projectId='+pid)
+      .then( response => response.json() )  // "a => expression" is shorthand function declaration
+      .then( json => {
+        dashboardApp.workHours = json;
+        this.formatWorkHours();
+        this.buildEffortChart();
+      } )
       .catch( err => {
-        console.log('PROJECT FETCH ERROR:');
+        console.log('PROJECT WORK FETCH ERROR:');
         console.log(err);
       })
     },
-
-    formatWorkHoursData() {
+    formatWorkHours() {
       this.workHours.forEach(
-        function(entry, index, arr) {
-          entry.date = Date.parse(entry.date);
+        (entry, index, arr) => {
+          entry.date = Date.parse(entry.date); // Convert to ms since Jan 1, 1970 UTC
           entry.hours = Number(entry.hours);
-          entry.runningTotalHours = entry.hours
-          + (index == 0 ? 0 : arr[index-1].runningTotalHours)
+          entry.runningTotalHours = entry.hours +
+            (index == 0 ? 0 : arr[index-1].runningTotalHours)
+      });
 
-        }
-      );
+      // DEBUG: Make sure the data is how we want it:
       console.log(this.workHours);
+    },
+    buildEffortChart() {
+      Highcharts.chart('effortChart', {
+            title: {
+                text: 'Cumulative Project Effort'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Hours'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+            series: [{
+                type: 'area',
+                name: 'Hours (Running Total)',
+                // Data needs [ [date, num], [date2, num2 ], ... ]
+                data: this.workHours.map( item => [item.date, item.runningTotalHours] )
+            }]
+        });
     },
     gotoTask(tid) {
       window.location = 'task.html?taskId=' + tid;
     }
   },
   created () {
-    // TODO: Get projectId from URL param
-          this.fetchProject();
-          this.fetchProjectWork(1);
-          this.fetchTasks();
+    // Get URL Param, projectId
+    const url = new URL(window.location.href);
+    const projectId = url.searchParams.get('projectId') || 0;
+
+    if (!projectId) {
+      console.error('Project Id not defined in URL parameters.')
+    }
+
+    this.project.id = projectId;
+    this.fetchProject(projectId);
+    this.fetchTasks(projectId);
+    this.fetchProjectWork(projectId);
   }
 })
